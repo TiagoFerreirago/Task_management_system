df5ea9962fbd86bcad3f7ddcee3348880d8860d6
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-05-05 20:27:11 -0300

    creation of the entity

---
+++ src/main/java/com/th/nextdone/model/task.java
@@ -0,0 +1,5 @@
+package com.th.nextdone.model;
+
+public class task {
+
+}
---
+++ src/test/java/com/th/nextdone/NextdoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.nextdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextdoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
