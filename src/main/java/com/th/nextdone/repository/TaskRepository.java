62b278832fc4357871ddb1e74e34d43542fa8899
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-05-08 13:29:52 -0300

    implementing search query by data

--- src/main/java/com/th/nextdone/repository/TaskRepository.java
+++ src/main/java/com/th/nextdone/repository/TaskRepository.java
@@ -1,9 +1,23 @@
 package com.th.nextdone.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.th.nextdone.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+	
+	//Pesquisar por Data
+	@Query("SELECT t From Task t WHERE t.dataCreation = :data")
+	List<Task> searchByDate(@Param(value = "date") LocalDate date);
+	//Pesquisar entre datas
+	@Query("SELECT t FROM Task t WHERE t.dataCreation BETWEEN :firstDate AND :lastDate")
+	List<Task> searchBeetwenPeriod(@Param(value = "firstDate")LocalDate firstDate, @Param(value = "lastDate")LocalDate lastDate);
+	//Pesquisar por status
+	@Query("SELECT t FROM Task t WHERE t.completed = :status")
+	List<Task> searchByStatus(@Param(value = "status")boolean status);
 }
