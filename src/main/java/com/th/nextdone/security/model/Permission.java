63a80a4daa5a06eb07c620cbf2d4667610a12d3d
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-05-09 19:29:13 -0300

    implementing permission entity

---
+++ src/main/java/com/th/nextdone/security/model/Permission.java
@@ -0,0 +1,68 @@
+package com.th.nextdone.security.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "permission")
+@Entity
+public class Permission implements Serializable,GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "description")
+	private String description;
+	
+	@Override
+	public String getAuthority() {
+		return this.description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Permission other = (Permission) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/th/nextdone/security/model/User.java
@@ -0,0 +1,5 @@
+package com.th.nextdone.security.model;
+
+public class User {
+
+}
