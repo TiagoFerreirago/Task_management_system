e5753a27190342ccfdfbf8f3ae478201b6b5b019
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-05-11 17:41:45 -0300

    Create SecurityFilterChain with rules for authentication, authorization and JWT

--- src/main/java/com/th/nextdone/security/config/SecurityConfig.java
+++ src/main/java/com/th/nextdone/security/config/SecurityConfig.java
@@ -5,12 +5,20 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.th.nextdone.security.jwt.JwtTokenFilter;
 import com.th.nextdone.security.jwt.JwtTokenProvider;
 
 @EnableWebSecurity
@@ -33,5 +41,27 @@ public class SecurityConfig {
 		
 		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
 		return passwordEncoder;
+	}
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		 return configuration.getAuthenticationManager();
+	}
+	
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+		return http.httpBasic(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
+				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests( request -> request
+						.requestMatchers(
+								"/auth/signin",
+								"auth/refresh/**",
+								"/swagger/ui/**",
+								"/v3/api-docs/**").permitAll()
+						.requestMatchers("/task/**").authenticated()
+						.requestMatchers("/users").denyAll())
+				.cors(cors -> {})
+				.build();
 	}
 }
